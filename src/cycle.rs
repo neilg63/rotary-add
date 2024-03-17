3f33dde92831f1219132e1367aca75dfc235311f
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2024-03-17 15:27:35 +0000

    edited notes

--- src/cycle.rs
+++ src/cycle.rs
@@ -2,19 +2,20 @@ use std::ops::{Add, Sub};
 
 /// Trait with methods for cyclical arithmetic with unsigned integers
 /// When additions or subtractions may otherwise overflow, 
-/// either falling below zero or above the type's maximum,
+/// either falling below zero or above the specified base, 
 /// the values rotate. This is mainly for use with cryptography
 pub trait CycleAdd<T: Add<Output = T> + Sub<Output = T> + PartialEq + Copy> {
   /// Add another unsigned integer and start from zero again should value overflow,
   /// but retaining the remainder 
-  /// With u8, 255.cycle_add(4) is thus 3
+  /// With a a base of 60 applied to a u8 value, 53.cycle_add(10, 60) is thus 3
   fn cycle_add(&self, other: &T, base: T) -> T;
 
-  /// Subtract another unsigned integer and start from the type's max value 
+  /// Subtract another unsigned integer and start from a specified base 
   /// if the target result may be otherwise be negative if it were a signed integer
-  /// e.g. with u8 the max is 255 (allowing for 256 values including zero)
-  /// 3.cycle_sub(4) is thus 255
+  /// e.g. if a base of 24 is applied to a u8 value, its max is 23
+  /// 3.cycle_sub(4) is thus 23
   fn cycle_sub(&self, other: &T, base: T) -> T;
+
 }
 
 /// Macro to implement the above for u8, u16 and u32
@@ -44,11 +45,11 @@ macro_rules! impl_cycle_add {
     };
 }
 
-// Implement for u8, max 255 (value range 256)
+// Implement for u8, max 255
 impl_cycle_add!(u8, u16);
 
-// Implement for u16, max 65535 (value range 65536)
+// Implement for u16, max 65535
 impl_cycle_add!(u16, u32);
 
-// Implement for u32, max 4294967295 (value range 4294967296)
+// Implement for u32, max 4294967295
 impl_cycle_add!(u32, u64);
