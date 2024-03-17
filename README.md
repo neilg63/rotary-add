91845c5f188c53eb04141e2ac2b6a604655f32b2
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2024-03-17 14:10:05 +0000

    added notes

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 
 # RotaryAdd: Cyclical arithemtic with unsigned integers
 
-This crate provides a trait with two simple methods, rotary_add() and rotary_sub(), to apply cyclical or modular arithmetic with three unsigned integer types, u8, u16 and u32. Unlike the default + and - operators, additions and subtractions never overflow. This example with an 8-bit unsigned integer illustrates the concept:
+This crate provides 2 traits, RotaryAdd and CyccleAdd, with two simple methods to apply cyclical or modular addition and subtraction with three unsigned integer types, u8, u16 and u32. Unlike the default + and - operators, additions and subtractions never overflow.
+
+## RotaryAdd
+
+This trait has addition and subtraction methods that act on the whole range of an unsigned integer, e.g. from *0 to 255* for *u8* or from *0 to 65535* for *u16*.
+This example with an 8-bit unsigned integer illustrates the concept with the rotary_add and rotary_sub methods:
 
 Addition:
 ```rust
@@ -22,6 +27,30 @@ let first_number: u8 = 3;
 let second_number: u8 = 6;
 let result = first_number.rotary_sub(&second_number);
 // yields 253. first_number - second_number would panic as the value would overflow
+```
+
+## CycleAdd
+
+This trait provides addition and subtraction methods that let you define a custom *cyclic base*.
+
+These examples show the concept:
+
+Addition:
+```rust
+let first_number: u8 = 22;
+let second_number: u8 = 6;
+
+let result = first_number.cycle_add(&second_number, 24);
+// yields 4. We have to use u8 as a primitive data type, but
+```
+
+Subtraction
+
+```rust
+let first_number: u8 = 37;
+let second_number: u8 = 78;
+let result = first_number.cycle_sub(&second_number, 100);
+// yields 59. first_number - second_number would panic as the value would overflow
 ```
 
 Unlike the related [Ring360](https://crates.io/crates/ring360) crate, this library only extends core unsigned integer types for use with cryptography and as a building block for other crates, e.g. converting characters first to u32 values and then shifting their values in one direction in the encoding stage and reversing the process in the decoding stage. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,12 @@
-use std::ops::{Add, Sub};
 
-/// Trait with methods for cyclical or rotary arithmetic with unsigned integers
-/// When additions or subtractions may otherwise overflow, 
-/// either falling below zero or above the type's maximum,
-/// the values rotate. This is mainly for use with cryptography
-pub trait RotaryAdd<T: Add<Output = T> + Sub<Output = T> + PartialEq + Copy> {
-  /// Add another unsigned integer and start from zero again should value overflow,
-  /// but retaining the remainder 
-  /// With u8, 255.rotary_add(4) is thus 3
-  fn rotary_add(&self, other: &T) -> T;
+mod rotary;
+mod cycle;
 
-  /// Subtract another unsigned integer and start from the type's max value 
-  /// if the target result may be otherwise be negative if it were a signed integer
-  /// e.g. with u8 the max is 255 (allowing for 256 values including zero)
-  /// 3.rotary_sub(4) is thus 255
-  fn rotary_sub(&self, other: &T) -> T;
-}
-/// Macro to implement the above for u8, u16 and u32
-macro_rules! impl_rotary_add {
-    ($t:ty,$u:ty, $max:expr) => {
-        impl RotaryAdd<$t> for $t {
-            fn rotary_add(&self, other: &$t) -> $t {
-                let diff = $max - *other;
-                if *self < diff {
-                    *self + *other
-                } else {
-                  let radix = $max as $u + 1;
-                  let result = *self as $u + *other as $u;
-                  (result % radix) as $t
-                }
-            }
+/// RotaryAdd works on the whole range of the unsigned integer
+pub use rotary::*;
 
-            fn rotary_sub(&self, other: &$t) -> $t {
-                if *self < *other {
-                  let radix = $max as $u + 1;
-                  let result = *other as $u - *self as $u;
-                  (radix - result) as $t
-                } else {
-                    *self - *other
-                }
-            }
-        }
-    };
-}
-
-// Implement for u8, max 255 (value range 256)
-impl_rotary_add!(u8, u16, u8::MAX);
-
-// Implement for u16, max 65535 (value range 65536)
-impl_rotary_add!(u16, u32, u16::MAX);
-
-// Implement for u32, max 4294967295 (value range 4294967296)
-impl_rotary_add!(u32, u64, u32::MAX);
+/// CycleAdd works on range from 0 to a a specified base. The max value is 1 less than the base
+pub use cycle::*;
 
 #[cfg(test)]
 mod tests {
@@ -128,4 +83,25 @@ mod tests {
     let expected = 4294967295;
     assert_eq!(result, expected);
   }
+
+
+  #[test]
+  fn test_cycle_add_u8() {
+    let num_1: u8 = 35;
+    let num_2: u8 = 29;
+    let result = num_1.cycle_add(&num_2, 60);
+    let expected = 4;
+    assert_eq!(result, expected);
+  }
+
+  #[test]
+  fn test_cycle_sub_u16() {
+    let num_1: u16 = 20_000;
+    let num_2: u16 = 24_000;
+    let base: u16 = 16_384;
+    let result = num_1.cycle_sub(&num_2, base);
+    let expected = 12384;
+    assert_eq!(result, expected);
+  }
+
 }
